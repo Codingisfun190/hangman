cd089e592053a2ff6629d8569f0d8891c41fe04a
Codingisfun190 <chanyoung720@gmail.com>
2023-01-19 13:00:49 -0500

    display blank cards for random word + got rid of correct guesses div + card styles

--- app.js
+++ app.js
@@ -3,15 +3,44 @@ document.addEventListener("DOMContentLoaded", () => {
   
   const userInput = document.getElementById("user-input");
   const submitButton = document.getElementById("submit")
-  const guessBankCorrect = document.querySelector(".guess-bank[data-type=correct]")
   const guessBankIncorrect = document.querySelector(".guess-bank[data-type=incorrect]");
   let guessedList = new Array;
 
+  // TODO  
+  // create word bank
+const wordBank = [
+  "apple",
+  "orange",
+  "banana",
+  "melon"
+]
+
+  // show a word to guess
+  const word = document.getElementById("word")
+  let randomIndex = Math.floor(Math.random() * wordBank.length);
+  // pick random word
+  let randomWord = wordBank[randomIndex].toUpperCase();
+  // show characters as blank card
+  let blankCards = "";
+  for (let i = 0; i < randomWord.length; i++) {
+    blankCards += `<p class="card"></p>`;
+  }
+  word.innerHTML = blankCards;
+
+  // show correct guesses
+  // show man hanging progression
+
+
+
+
+
+
   // prevent page from refreshing
   submitButton.addEventListener("click", (event) => {
+    // prevent page from refreshing
     event.preventDefault();
     event.stopPropagation();
-      // get user's guess
+    // get user's guess
   let guessedLetter = userInput.value.toUpperCase();
 
   // validate not empty
@@ -23,7 +52,7 @@ document.addEventListener("DOMContentLoaded", () => {
     guessedList.push(guessedLetter);
 
     guessBankIncorrect.innerHTML += `<p class="card">${guessedList[guessedList.length - 1]}</p>`;
-}
+  }
 
 
   // remove letter after guessing && focus on userInput
@@ -38,7 +67,7 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log(guessedList.length);
   
 
-  }) 
+  })  
  
 
 
