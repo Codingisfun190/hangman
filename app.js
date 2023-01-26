29a5b08a50f16b9212e944464d63492cbd484995
Codingisfun190 <chanyoung720@gmail.com>
2023-01-26 12:52:27 -0500

    handle guess function, validation issues

--- app.js
+++ app.js
@@ -36,40 +36,10 @@ const wordBank = [
     // prevent page from refreshing
     event.preventDefault();
     event.stopPropagation();
-    // get user's guess
-  let guessedLetter = userInput.value.toUpperCase();
 
-  // validate not empty
-  // only add unique letters -- no duplicates
-  // TODO only allow letters -- no special characters or numbers
-  if (guessedLetter && !guessedList.includes(guessedLetter)) {
-
-    // handle guesses
-    if (randomWord.includes(guessedLetter)) {
-      // show correct letters
-
-      //TODO fix resetting previously correct guesses
-      let tempCards = "";
-      for (let i = 0; i < randomWord.length; i++) {
-        if (randomWord[i] === guessedLetter) {
-          tempCards += `<p class="card">${guessedLetter}</p>`
-        } else {
-          tempCards += `<p class="card"></p>`
-        }
-      }
-      word.innerHTML = tempCards;
-
-      
-    } else {
-      // append to incorrect list
-    guessedList.push(guessedLetter);
-    // Loop through list to display guessed letters
-    guessBankIncorrect.innerHTML += `<p class="card">${guessedList[guessedList.length - 1]}</p>`;
-    }
-
-    // TODO check win/lose
-
-  }
+    handleGuess();
+    handleWinLose();
+    
 
   // remove letter after guessing && focus on userInput
   userInput.value ="";
@@ -81,6 +51,64 @@ const wordBank = [
   
   })  
  
+  function handleGuess () {
+    // get user's guess
+    let guessedLetter = userInput.value.toUpperCase();
+  
+    // validate not empty
+    // only add unique letters -- no duplicates
+    // TODO only allow letters -- no special characters or numbers
+    if (guessedLetter && !guessedList.includes(guessedLetter)) {
+  
+      // handle guesses
+      if (randomWord.includes(guessedLetter)) {
+        // show correct letters
+  
+        //TODO fix resetting previously correct guesses
+        // for (let i = 0; i < randomWord.length; i++) {
+        //   if (word[i].innerText === guessedLetter) {
+        //     tempCards += `<p class="card">${guessedLetter}</p>`
+        //   } else {
+        //     tempCards += `<p class="card"></p>`
+        //   }
+        // }
+        // word.innerHTML = tempCards;
+  
+        Array.from(word.children).forEach((letter, i) => {
+          // check if blank
+          if(!letter.innerText) {
+            // update correct letters
+            if (randomWord[i] === guessedLetter) {
+              word[i] = `<p class="card">${guessedLetter}</p>`;
+              letter.innerText= guessedLetter
+            }
+          }
+        })
+  
+        
+  
+      } else {
+        // append to incorrect list
+      guessedList.push(guessedLetter);
+      // Loop through list to display guessed letters
+      guessBankIncorrect.innerHTML += `<p class="card">${guessedList[guessedList.length - 1]}</p>`;
+      }
+  
+      
+  
+    }
+  }
+
+  function handleWinLose () {
+    let hasBlank = false;
+   Array.from(word.children).forEach(letter => {
+    if(!letter.innerText) {
+      hasBlank = true;
+      console.log(hasBlank)
+    }
+   })
+  }
+
 });
 
 
